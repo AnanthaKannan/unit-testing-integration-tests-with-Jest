a34849577d7166222f3245ecaf674634223c3dfe
AnanthaKannan <sreeananthakannan@gmail.com>
2021-06-08 19:53:30 +0530

    code coverage script added

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-async function connect(){
-    const mongoUrl = "mongodb+srv://chettinad:chettinad@cluster0.jssug.mongodb.net/chettinad?retryWrites=true&w=majority";
-    try{
-    await mongoose.connect(mongoUrl, { useNewUrlParser: true,  useUnifiedTopology: true  });
-    }
-    catch(e){
-        console.error(`Error to connect ${mongoUrl}`)
-        console.error(e);
-    }
-}
-// 
+const connect = async () => {
+  const mongoUrl =
+    "mongodb+srv://chettinad:chettinad@cluster0.jssug.mongodb.net/chettinad?retryWrites=true&w=majority";
+  try {
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.error(`Error to connect ${mongoUrl}`);
+    console.error(e);
+  }
+};
+//
 
-module.exports = { connect}
+module.exports = { connect };
